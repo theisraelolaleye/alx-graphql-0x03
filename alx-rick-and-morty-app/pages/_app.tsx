dbbcf70f6e96a53716ef422f80aa2e4e3d87b188
theisraelolaleye <theisraelolaleye@gmail.com>
2025-10-21 08:08:58 +0100

    feat: integrate ErrorBoundary and temporarily disable ApolloProvider
    - Add ErrorBoundary component to wrap the entire app
    - Comment out ApolloProvider temporarily for testing
    - Import ErrorBoundary component in _app.tsx
    - Ensure error boundary catches all app-level errors

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -2,11 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client/react";
 import client from "@/graphql/apolloClient";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    // <ApolloProvider client={client}>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    // </ApolloProvider>
   )
 }
